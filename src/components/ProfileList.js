49a3e30d85367ad5968a45371c3e4470d7da76f4
Chinwendu joy Enyidiegwu <chinwe640@gmail.com>
2023-09-13 18:18:07 +0100

    changes

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import Profile from './Profile'
 import { profiles } from '../profile-data'
-
+import styles from './ProfileList.module.css'
 
 const ProfileList = () => {
-  return (
-		<div >
-    {profiles.map((profile)=>{
-      return (
-				<Profile
-					img={profile.img}
-					name={profile.name}
-					job={profile.job}
-					company={profile.company}
-					link={profile.link}
-				/>
-			)
-    })}
-
-		
-		</div>
+	return (
+		<section>
+			<div>
+				<h1>Team Members</h1>
+				<div className={styles['profile-container']}>
+					{profiles.map((profile, index) => {
+						const { img, name, job, company, link } = profile
+						return (
+							<Profile
+								key={index}
+								img={img}
+								name={name}
+								job={job}
+								company={company}
+								link={link}
+							/>
+						)
+					})}
+				</div>
+			</div>
+		</section>
 	)
 }
 
